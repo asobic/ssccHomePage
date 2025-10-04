76fd045aa5acd2eff1e7c78e195eb393b80799a0
Naoo122 <kingfishernao2017@gmail.com>
2025-10-04 19:47:05 +0900

    Update Node.js

--- Node.js
+++ Node.js
@@ -2,22 +2,32 @@ document.addEventListener("DOMContentLoaded", function () {
   const HomeText = document.getElementById('Home');
   const DayOfSSCC = document.getElementById("The_Day_Of_SSCC");
   const RoomOfMembers = document.getElementById("Page_For_Members");
+  const Acticity = document.getElementById("Activity");
   
   window.ToHome = function(){
     HomeText.hidden = false;
     DayOfSSCC.hidden = true;
     RoomOfMembers.hidden = true;
+    Activity.hidden = true;
   }
   
   window.ToDayOfSSCC = function(){
     HomeText.hidden = true;
     DayOfSSCC.hidden = false;
     RoomOfMembers.hidden = true;
+    Activity.hidden = true;
   }
   
   window.ToRoomOfMembers = function(){
     HomeText.hidden = true;
     DayOfSSCC.hidden = true;
     RoomOfMembers.hidden = false;
+    Activity.hidden = true;
   }
+
+  window.ToActicity = function(){
+    HomeText.hidden = true;
+    DayOfSSCC.hidden = true;
+    RoomOfMembers = true;
+    Activity.hidden = false;
 });
