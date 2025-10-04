80dc2e5f26a4ea72e78c2c2b444cf670dd491817
Naoo122 <kingfishernao2017@gmail.com>
2025-10-04 18:44:10 +0900

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,22 +1,23 @@
-const HomeText = document.getElementById('Home');
-const DayOfSSCC = document.getElementById("The_Day_Of_SSCC");
-const RoomOfMembers = document.getElementById("Page_For_Members");
-
-function ToHome(){
-　HomeText.hidden = false;
-  DayOfSSCC.hidden = true;
-  RoomOfMembers.hidden = true;
-}
-
-function ToDayOfSSCC(){
-  HomeText.hidden = true;
-  DayOfSSCC.hidden = false;
-  RoomOfMembers.hidden = true;
-}
-
-function ToRoomOfMenbers(){
-  HomeText.hidden = true;
-  DayOfSSCC.hidden = true;
-  RoomOfMembers.hidden = false;
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const HomeText = document.getElementById('Home');
+  const DayOfSSCC = document.getElementById("The_Day_Of_SSCC");
+  const RoomOfMembers = document.getElementById("Page_For_Members");
   
+  function ToHome(){
+  　HomeText.hidden = false;
+    DayOfSSCC.hidden = true;
+    RoomOfMembers.hidden = true;
+  }
+  
+  function ToDayOfSSCC(){
+    HomeText.hidden = true;
+    DayOfSSCC.hidden = false;
+    RoomOfMembers.hidden = true;
+  }
+  
+  function ToRoomOfMenbers(){
+    HomeText.hidden = true;
+    DayOfSSCC.hidden = true;
+    RoomOfMembers.hidden = false;
+  }
+}
