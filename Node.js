28a9f41c0f8e7ad8dfc6828275fd510cfa4fdbca
asobic <cat600073@gmail.com>
2025-10-09 15:04:49 +0900

    Node.js を更新

--- Node.js
+++ Node.js
@@ -1,46 +1,50 @@
-  const HomeText = document.getElementById('Home');
-  const DayOfSSCC = document.getElementById("The_Day_Of_SSCC");
-  const RoomOfMembers = document.getElementById("Page_For_Members");
-  const Activity = document.getElementById("Activity");
-  const Menutab = document.getElementById('menutab');
-  const Menuicon = document.getElementById('menuicon');
+const HomeText = document.getElementById('Home');
+const DayOfSSCC = document.getElementById("The_Day_Of_SSCC");
+const RoomOfMembers = document.getElementById("Page_For_Members");
+const Activity = document.getElementById("Activity");
+const Menutab = document.getElementById('menutab');
+const Menuicon = document.getElementById('menuicon');
 
-
-  window.onload =function(){
-    HomeText.hidden = false;
-    DayOfSSCC.hidden = true;
-    RoomOfMembers.hidden = true;
-    Activity.hidden = true;
-  }
-  
-  window.ToHome = function(){
-    HomeText.hidden = false;
-    DayOfSSCC.hidden = true;
-    RoomOfMembers.hidden = true;
-    Activity.hidden = true;
-  }
-  
-  window.ToDayOfSSCC = function(){
-    HomeText.hidden = true;
-    DayOfSSCC.hidden = false;
-    RoomOfMembers.hidden = true;
-    Activity.hidden = true;
-  }
-  
-  window.ToRoomOfMembers = function(){
-    HomeText.hidden = true;
-    DayOfSSCC.hidden = true;
-    RoomOfMembers.hidden = false;
-    Activity.hidden = true;
-  }
-
-  window.ToActivity = function(){
-    HomeText.hidden = true;
-    DayOfSSCC.hidden = true;
-    RoomOfMembers.hidden = true;
-    Activity.
+window.onload = function() {
+  HomeText.hidden = false;
+  DayOfSSCC.hidden = true;
+  RoomOfMembers.hidden = true;
+  Activity.hidden = true;
 }
+
+window.ToHome = function(){
+  HomeText.hidden = false;
+  DayOfSSCC.hidden = true;
+  RoomOfMembers.hidden = true;
+  Activity.hidden = true;
+}
+
+window.ToDayOfSSCC = function(){
+  HomeText.hidden = true;
+  DayOfSSCC.hidden = false;
+  RoomOfMembers.hidden = true;
+  Activity.hidden = true;
+}
+
+window.ToRoomOfMembers = function(){
+  HomeText.hidden = true;
+  DayOfSSCC.hidden = true;
+  RoomOfMembers.hidden = false;
+  Activity.hidden = true;
+}
+
+window.ToActivity = function(){
+  HomeText.hidden = true;
+  DayOfSSCC.hidden = true;
+  RoomOfMembers.hidden = true;
+  Activity.hidden = false;
+}
+
 Menuicon.addEventListener('click', function() {
-  Menutab.classList.toggle('show');
+  if (Menutab.style.display === 'block') {
+    Menutab.style.display = 'none';
+  } else {
+    Menutab.style.display = 'block';
+  }
 });
 
