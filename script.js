55a7d281b7ade84de32a46edd3155d29cec29d52
asobic <cat600073@gmail.com>
2025-10-14 18:09:04 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
-  (async function () {
-    const Home = document.getElementById("Home");
-    const DayOfSSCC = document.getElementById("The_Day_Of_SSCC");
-    const Activity = document.getElementById("Activity");
-    const RoomOfMembers = document.getElementById("Page_For_Members");
-    const Menutab = document.getElementById('menutab');
-    const Menuicon = document.getElementById('menuicon');
-   Menuicon.addEventListener('click', function() {
-  if (Menutab.style.display === 'block') {
-    Menutab.style.display = 'none';
-  } else {
-    Menutab.style.display = 'block';
-  }
-});
+(function () {
+  const Home = document.getElementById("Home");
+  const DayOfSSCC = document.getElementById("The_Day_Of_SSCC");
+  const Activity = document.getElementById("Activity");
+  const RoomOfMembers = document.getElementById("Page_For_Members");
+  const Menutab = document.getElementById('menutab');
+  const Menuicon = document.getElementById('menuicon');
 
-    function showSection(sectionId, bool) {
-      const sections = [Home, DayOfSSCC, Activity, RoomOfMembers];
-      sections.forEach((sec) => (sec.hidden = sec.id !== sectionId));
-      if (bool){
-        Menuicon.style.display = 'none';}
+  Menuicon.addEventListener('click', function() {
+    if (Menutab.style.display === 'block') {
+      Menutab.style.display = 'none';
+    } else {
+      Menutab.style.display = 'block';
     }
+  });
 
-    showSection("Home",False);
+  function showSection(sectionId, hideMenuIcon) {
+    const sections = [Home, DayOfSSCC, Activity, RoomOfMembers];
+    sections.forEach((sec) => (sec.hidden = sec.id !== sectionId));
+    if (hideMenuIcon) {
+      Menuicon.style.display = 'none';
+    } else {
+      Menuicon.style.display = 'flex';
+    }
+    Menutab.style.display = 'none'; // メニューは閉じる
+  }
 
-    window.ToHome = () => showSection("Home",True);
-    window.ToDayOfSSCC = () => showSection("DayOfSSCC",True);
-    window.ToRoomOfMembers = () => showSection("RoomOfMembers",True);
-    window.ToActivity = () => showSection("Activity",True);
-  })();
+  showSection("Home", false);
+
+  window.ToHome = () => showSection("Home", true);
+  window.ToDayOfSSCC = () => showSection("The_Day_Of_SSCC", true);
+  window.ToRoomOfMembers = () => showSection("Page_For_Members", true);
+  window.ToActivity = () => showSection("Activity", true);
+})();
